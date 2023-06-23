47a3e176adb66b496c316ae1def3e9853c9c6d29
mi3765 <chips1225@outlook.jp>
2023-06-23 13:36:13 +0900

    フォルダー整理

---
+++ ABC303/PB_303.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> a(m, vector<int> (n));
+    for(int i = 0; i < m; i++) for(int j = 0; j < n; j++) cin >> a[i][j];
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(a)
+        }
+    }
+    return 0;
+}
