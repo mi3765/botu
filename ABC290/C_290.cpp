f5c93e42b82fd5bf51cd635fb35ae1c6b2c8e0ef
mi3765 <chips1225@outlook.jp>
2023-06-23 15:31:51 +0900

    C_290.cppを新規作成

---
+++ ABC290/C_290.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n, k;
+    cin >> n >> k;
+    set<int> box;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+        box.insert(a[i]);
+    }
+
+    
+    return 0;
+}
