625da159494804cfbd61faac6ad39eab5d27a4bd
mi3765 <chips1225@outlook.jp>
2023-06-24 14:27:18 +0900

    A_282.cppを編集

--- ABC282/A_282.cpp
+++ ABC282/A_282.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    
+    int k;
+    cin >> k;
+    for(int i = 0; i < k; i++) {
+        int c = 'A' + i;
+        cout << (char)c;
+        c = 0;
+    }
     return 0;
 }
